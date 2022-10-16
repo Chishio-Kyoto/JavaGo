53bad275286a55511b7c359d2ebcdaae5dd1ed41
Chishio-Kyoto <houle.cf@outlook.com>
2022-10-16 16:41:06 +0800

    Java Coding Basics Sample

---
+++ JavaCoding/Sample05/xml-json/src/main/java/Sample03.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Sample03 {
+    public static void main(String[] args) throws IOException {
+        InputStream input = Sample03.class.getResourceAsStream("/book.xml");
+        JacksonXmlModule module = new JacksonXmlModule();
+        XmlMapper mapper = new XmlMapper(module);
+        Book book = mapper.readValue(input, Book.class);
+        System.out.println(book.id);
+        System.out.println(book.name);
+        System.out.println(book.author);
+        System.out.println(book.isbn);
+        System.out.println(book.tags);
+        System.out.println(book.pubDate);
+    }
+}
